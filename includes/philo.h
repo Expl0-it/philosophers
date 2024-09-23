311c26301541905996084c6dec9074657cae27a1
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-09-23 12:57:58 +0200

    started protptyping the header file

---
+++ includes/philo.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/17 17:51:07 by mamichal          #+#    #+#             */
+/*   Updated: 2024/09/23 12:56:42 by mamichal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+// INCLUDES
+# include "../libft/libft.h"
+# include <errno.h>
+
+// TYPE DECLARATIONS
+typedef enum e_errors
+{
+	OK = 0,
+	MALLOC_ERROR = 1,
+	NOT_ENOUGH_ARGS = 2,
+	ERR_ARGS = 3,
+}				t_errors;
+
+// FUNCTION DECLARATIONS
+
+#endif
