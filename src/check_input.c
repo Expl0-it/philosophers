9f1c215a804c90b4b5641c7dc351683a25873caa
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-09-26 14:02:33 +0200

    created check_input fn

--- src/check_input.c
+++ src/check_input.c
@@ -6,13 +6,13 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 15:52:27 by mamichal          #+#    #+#             */
-/*   Updated: 2024/09/26 12:10:15 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/09/26 14:01:58 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-bool	check_content(char *arg)
+static bool	check_content(char *arg)
 {
 	int	i;
 
@@ -26,9 +26,21 @@ bool	check_content(char *arg)
 	return (true);
 }
 
-bool	check_input(t_table *table, int argc, char **argv)
+bool	check_input(int argc, char **argv)
 {
 	if (argc != 5 || argc != 6)
-		return (false);
-
+		return (write(2, "Ivalid number of arguments\n", 27), false);
+	if (ft_atoi(argv[1]) > PHILO_MAX || ft_atoi(argv[1]) <= 0
+		|| check_content(argv[1]) == false)
+		return (write(2, "Invalid number of philos\n", 25), false);
+	if (ft_atoi(argv[2]) <= 0 || check_content(argv[2]) == false)
+		return (write(2, "Invalid time to die\n", 21), false);
+	if (ft_atoi(argv[3]) <= 0 || check_content(argv[3]) == false)
+		return (write(2, "Invalid time to eat\n", 21), false);
+	if (ft_atoi(argv[4]) <= 0 || check_content(argv[4]) == false)
+		return (write(2, "Invalid time to sleep\n", 23), false);
+	if (NULL != argv[5] && (ft_atoi(argv[3]) < 0
+			|| check_content(argv[3]) == false))
+		return (write(2, "Invalid times philos should eat\n", 32), false);
+	return (true);
 }
