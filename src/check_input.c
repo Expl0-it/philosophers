30031fa895f9976ac471f52cc1f7561ed2cdbf90
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-09-29 16:22:04 +0200

    updated fn  check_input() to use printf insted of write while logging

--- src/check_input.c
+++ src/check_input.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 15:52:27 by mamichal          #+#    #+#             */
-/*   Updated: 2024/09/27 18:12:09 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/09/29 16:14:54 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,18 +29,18 @@ static bool	check_content(char *arg)
 bool	check_input(int argc, char **argv)
 {
 	if (argc != 5 && argc != 6)
-		return (write(2, "Ivalid number of arguments\n", 27), false);
+		return (printf("Ivalid number of arguments\n"), false);
 	if (ft_atoi(argv[1]) > PHILO_MAX || ft_atoi(argv[1]) <= 0
 		|| check_content(argv[1]) == false)
-		return (write(2, "Invalid number of philos\n", 25), false);
+		return (printf("Invalid number of philos\n"), false);
 	if (ft_atoi(argv[2]) <= 0 || check_content(argv[2]) == false)
-		return (write(2, "Invalid time to die\n", 21), false);
+		return (printf("Invalid time to die\n"), false);
 	if (ft_atoi(argv[3]) <= 0 || check_content(argv[3]) == false)
-		return (write(2, "Invalid time to eat\n", 21), false);
+		return (printf("Invalid time to eat\n"), false);
 	if (ft_atoi(argv[4]) <= 0 || check_content(argv[4]) == false)
-		return (write(2, "Invalid time to sleep\n", 23), false);
+		return (printf("Invalid time to sleep\n"), false);
 	if (NULL != argv[5] && (ft_atoi(argv[5]) < 0
 			|| false == check_content(argv[5])))
-		return (write(2, "Invalid times philos should eat\n", 32), false);
+		return (printf("Invalid times philos should eat\n"), false);
 	return (true);
 }
