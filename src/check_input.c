cd7c74fd613fcbb1ad2fe7b723a198383f34b99e
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-09-30 10:00:29 +0200

    changed parse fn declaration to use and actual pointer and not a value

--- src/check_input.c
+++ src/check_input.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 15:52:27 by mamichal          #+#    #+#             */
-/*   Updated: 2024/09/29 19:40:32 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/09/30 09:59:46 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ static bool	check_content(char *arg)
 	return (true);
 }
 
+// TODO: check for max int???
 bool	check_input(int argc, char **argv)
 {
 	if (argc != 5 && argc != 6)
@@ -45,14 +46,14 @@ bool	check_input(int argc, char **argv)
 	return (true);
 }
 
-void	parse(t_table p_table, char **argv)
+void	parse(t_table *p_table, char **argv)
 {
-	p_table.philo_count = ft_atoi(argv[1]);
-	p_table.time_to_die = ft_atoi(argv[2]) * 1e3;
-	p_table.time_to_eat = ft_atoi(argv[3]) * 1e3;
-	p_table.time_to_sleep = ft_atoi(argv[4]) * 1e3;
+	p_table->philo_count = ft_atoi(argv[1]);
+	p_table->time_to_die = ft_atoi(argv[2]) * 1e3;
+	p_table->time_to_eat = ft_atoi(argv[3]) * 1e3;
+	p_table->time_to_sleep = ft_atoi(argv[4]) * 1e3;
 	if (NULL != argv[5])
-		p_table.meals_limit = ft_atoi(argv[5]);
+		p_table->meals_limit = ft_atoi(argv[5]);
 	else
-		p_table.meals_limit = -1;
+		p_table->meals_limit = -1;
 }
