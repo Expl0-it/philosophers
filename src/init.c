47e2bf4a874bdb66c788caf791f3e2b4afd1ce06
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-10-05 18:08:20 +0200

    added neccesary statements to init.c

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 09:57:07 by mamichal          #+#    #+#             */
-/*   Updated: 2024/10/05 17:43:40 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/10/05 18:08:00 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,15 +68,18 @@ static bool	philo_init(t_table *p_table)
 		philo->p_table = p_table;
 		philo->eating = false;
 		philo->last_meal = -1;
-		// TODO: ASSIGN FORKS
+		assign_forks(philo, p_table->forks, i);
 		i++;
 	}
+	return (true);
 }
 
 bool	data_init(t_table *p_table)
 {
 	p_table->end_flag = false;
 	if (false == fork_init(p_table))
+		return (false);
+	if (false == philo_init(p_table))
 		return (false);
 	return (true);
 }
