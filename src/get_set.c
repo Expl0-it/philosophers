fa379e5fe42202d4b9d77b606cb58192c4b1dede
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-10-07 09:24:43 +0200

    added fn for getting and setting mutex protected variables

---
+++ src/get_set.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   getters_setters.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 08:56:06 by mamichal          #+#    #+#             */
+/*   Updated: 2024/10/07 09:24:05 by mamichal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+int	get_int(pthread_mutex_t *mtx, int *source)
+{
+	int	ret;
+
+	handle_mutex(mtx, LOCK);
+	if (NULL != source)
+		ret = *source;
+	handle_mutex(mtx, UNLOCK);
+	return (ret);
+}
+
+bool	set_int(pthread_mutex_t *mtx, int *dest, int value)
+{
+	if (false == handle_mutex(mtx, LOCK))
+		return (false);
+	if (NULL != dest)
+		*dest = value;
+	if (false == handle_mutex(mtx, UNLOCK))
+		return (false);
+	return (true);
+}
+
+bool	get_bool(pthread_mutex_t *mtx, bool *source)
+{
+	bool	ret;
+
+	handle_mutex(mtx, LOCK);
+	if (NULL != source)
+		ret = *source;
+	handle_mutex(mtx, UNLOCK);
+	return (ret);
+}
+
+bool	set_bool(pthread_mutex_t *mtx, bool *dest, bool value)
+{
+	if (false == handle_mutex(mtx, LOCK))
+		return (false);
+	if (NULL != dest)
+		*dest = value;
+	if (false == handle_mutex(mtx, UNLOCK))
+		return (false);
+	return (true);
+}
