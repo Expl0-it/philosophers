11fa80f2e941feb54d316d72e7c16caeb595dbed
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-10-08 11:14:47 +0200

    moved creating threads in dinner.c to a different fn

--- src/dinner.c
+++ src/dinner.c
@@ -6,26 +6,38 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 18:08:42 by mamichal          #+#    #+#             */
-/*   Updated: 2024/10/08 11:03:55 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/10/08 11:14:28 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-bool	dinner_start(t_table *p_table)
+static bool	create_threads(t_table *p_table)
 {
 	int	i;
 
-	i = -1;
+	i = 0;
+
+	while (++i < p_table->philo_count)
+	{
+		if (false == handle_thread(p_table->philos[i].thread, \
+				philo_routine, &p_table->philos[i], CREATE))
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
+bool	dinner_start(t_table *p_table)
+{
 	if (0 == p_table->meals_limit)
 		return (true);
 	else if (1 == p_table->meals_limit)
 		;// TODO:
 	else
 	{
-		while (++i < p_table->philo_count)
-			if (false == handle_thread(p_table->philos[i].thread, \
-					philo_routine, &p_table->philos[i], CREATE))
-				return (false);
+		if (false == create_threads(p_table))
+			return (false);
+
 	}
 }
