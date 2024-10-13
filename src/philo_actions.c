971bded169e3bae4c9f332d395db0fa8be3138ea
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-10-13 17:18:38 +0200

    written fn to handle philo eating

--- src/philo_actions.c
+++ src/philo_actions.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 15:50:05 by mamichal          #+#    #+#             */
-/*   Updated: 2024/10/13 16:39:49 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/10/13 17:18:16 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,28 @@ bool	philo_sleep(t_philo *philo)
 	if (false == write_philo_status(philo, SLEEPING))
 		return (false);
 	precise_usleep(philo->p_table->time_to_sleep, philo->p_table);
+	return (true);
+}
+
+bool	philo_eat(t_philo *philo)
+{
+	if (false == handle_mutex(&philo->first_fork->fork, LOCK)
+			|| false == write_philo_status(philo, TOOK_1_FORK))
+		return (false);
+	if (false == handle_mutex(&philo->second_fork->fork, LOCK)
+			|| false == write_philo_status(philo, TOOK_2_FORK))
+		return (false);
+	if (false == set_long(&philo->philo_mtx, &philo->last_meal, get_time(MILLISECOND)))
+		return (false);
+	philo->meals_eaten++;
+	if (false == write_philo_status(philo, EATING))
+		return (false);
+	precise_usleep(philo->p_table->time_to_eat, philo->p_table);
+	if (philo->p_table->meals_limit > 0 && philo->meals_eaten > philo->p_table->meals_limit)
+		if (false == set_bool(&philo->philo_mtx, &philo->full, true))
+			return (false);
+	if (false == handle_mutex(&philo->first_fork->fork, UNLOCK)
+			|| false == handle_mutex(&philo->second_fork->fork, UNLOCK))
+		return (false);
 	return (true);
 }
