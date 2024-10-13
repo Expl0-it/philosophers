b865418593ffefe11f0f30cadb0a59858c455d60
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-10-13 18:14:07 +0200

    added neccesary statements to main for testing prourposes

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 15:48:42 by mamichal          #+#    #+#             */
-/*   Updated: 2024/10/08 10:43:04 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/10/13 17:34:23 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,17 @@ int	main(int argc, char **argv)
 
 	if (false == check_input(argc, argv))
 		return (INVALID_ARGS);
+	parse(&table, argv);
 	if (false == data_init(&table))
 	{
 		// TODO: CLEANUP
 		return (INIT_ERROR);
 	}
+	if (false == dinner_start(&table))
+	{
+		// TODO: CLEANUP
+		return (DINNER_ERROR);
+	}
+	// TODO: CLEANUP
 	return (OK);
 }
