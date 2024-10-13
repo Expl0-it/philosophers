8aab81b6e48469fe032930671901119f4d9d6eae
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-10-13 20:50:25 +0200

    now printing philo indexes starting from one, ending at philo_count

--- src/write.c
+++ src/write.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 13:38:17 by mamichal          #+#    #+#             */
-/*   Updated: 2024/10/13 18:15:37 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/10/13 20:49:50 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,18 @@ bool	write_philo_status(t_philo *philo, t_philo_status status)
 	elapsed = get_time(MILLISECOND) - philo->p_table->time_start;
 	if ((TOOK_1_FORK == status || TOOK_2_FORK == status)
 			&& false == get_end(philo->p_table))
-		printf("%-6ld %d has taken a fork\n", elapsed, philo->id);
+		printf("%-6ld %d has taken a fork\n", elapsed, philo->id + 1);
 	else if (EATING == status && false == get_end(philo->p_table))
-		printf("%-6ld %d is eating\n", elapsed, philo->id);
+		printf("%-6ld %d is eating\n", elapsed, philo->id + 1);
 	else if (SLEEPING == status && false == get_end(philo->p_table))
-		printf("%-6ld %d is sleeping\n", elapsed, philo->id);
+		printf("%-6ld %d is sleeping\n", elapsed, philo->id + 1);
 	else if (THINGKING == status && false == get_end(philo->p_table))
-		printf("%-6ld %d is thinking\n", elapsed, philo->id);
+		printf("%-6ld %d is thinking\n", elapsed, philo->id + 1);
 	else if (DEAD == status && false == get_end(philo->p_table))
-		printf("%-6ld %d died\n", elapsed, philo->id);
+		printf("%-6ld %d died\n", elapsed, philo->id + 1);
+	// NOTE: ADDITIONAL CASE FOR FULL
+	else if (FULL == status && false == get_end(philo->p_table))
+		printf("%-6ld %d is full\n", elapsed, philo->id + 1);
 	if (false == handle_mutex(&philo->p_table->write_mtx, UNLOCK))
 		return (false);
 	return (true);
