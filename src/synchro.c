d66021033b17df88f3be99d13902b68026f2275b
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-10-16 14:09:24 +0200

    added a fn to check if all the philosopher threads are running

--- src/synchro.c
+++ src/synchro.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 11:26:51 by mamichal          #+#    #+#             */
-/*   Updated: 2024/10/08 11:30:00 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/10/16 14:08:49 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,17 @@ void	wait_threads(t_table *p_table)
 {
 	while (false == get_bool(&p_table->table_mtx, &p_table->threads_ready))
 		;
+}
+
+bool	all_threads_active(pthread_mutex_t *mtx, long *nb_threads, \
+				int philo_count)
+{
+	bool	ret;
+
+	ret = false;
+	handle_mutex(mtx, LOCK);
+	if (*nb_threads == (long)philo_count)
+		ret = true;
+	handle_mutex(mtx, UNLOCK);
+	return (ret);
 }
