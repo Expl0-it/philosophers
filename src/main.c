59d01de004b587c99e7e0431085803320b26208c
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2024-10-20 20:16:39 +0200

    added cleanup fn to main, removed unused variable

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mamichal <mamichal@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 15:48:42 by mamichal          #+#    #+#             */
-/*   Updated: 2024/10/13 17:34:23 by mamichal         ###   ########.fr       */
+/*   Updated: 2024/10/20 20:15:11 by mamichal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,20 @@
 int	main(int argc, char **argv)
 {
 	t_table		table;
-	t_philo		philos[PHILO_MAX];
 
 	if (false == check_input(argc, argv))
 		return (INVALID_ARGS);
 	parse(&table, argv);
 	if (false == data_init(&table))
 	{
-		// TODO: CLEANUP
+		cleanup(&table);
 		return (INIT_ERROR);
 	}
 	if (false == dinner_start(&table))
 	{
-		// TODO: CLEANUP
+		cleanup(&table);
 		return (DINNER_ERROR);
 	}
-	// TODO: CLEANUP
+	cleanup(&table);
 	return (OK);
 }
